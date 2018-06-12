960e0c5ac84c103bb9ff676a90638577b7668e71
STRUCTUREIT\gabriel.groener <gabriel.groener@gmail.com>
2018-06-12 15:54:47 +0200

    done for day 2

--- Udemy_java/Strings/src/com/gabriel/Main.java
+++ Udemy_java/Strings/src/com/gabriel/Main.java
@@ -3,6 +3,24 @@ package com.gabriel;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        String myString = "This is a String.\n";
+        System.out.println(myString);
+        // this is how to append to the string
+        myString = myString + "This has been added afterwards";
+        System.out.println(myString);
+        myString = myString + " \u00AE 2018";
+        System.out.println(myString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("This is the lastString output: " + lastString);
+
+        // To address this issue we can use
+        myInt = myInt + Integer.parseInt(lastString);
+        lastString = "" + myInt;
+        System.out.println(lastString);
+
     }
 }
