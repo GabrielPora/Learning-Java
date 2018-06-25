a6ed0dc1a6cbbe0bfc9764015d81d2c98302a814
GabrielPora <gabriel.groener@gmail.com>
2018-06-25 06:21:09 +0200

    Start of Min Max Challenge

--- Udemy_java/MinAndMaxChallenge/src/com/gabriel/Main.java
+++ Udemy_java/MinAndMaxChallenge/src/com/gabriel/Main.java
@@ -5,28 +5,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner scannerInstructor = new Scanner(System.in);
         int sum = 0;
         int input = 0;
+        int min = 10;
+        int max = -10;
         int count = 1;
-        System.out.println("Please enter 10 numbers");
+        System.out.println("Please enter a set of numbers, Will return the max and min of those numbers entered");
         while (true) {
             System.out.println("Enter number #" + count);
             boolean isAnInt = scannerInstructor.hasNextInt();
             if (isAnInt) {
                 input = scannerInstructor.nextInt();
+                if (input<min){
+                    min = input;
+                }
+                if (input>max){
+                    max = input;
+                }
                 sum += input;
                 count++;
                 if (count > 10) {
                     break;
                 }
-            }else {
+            } else {
                 System.out.println("Invalid Number!\n Please Enter a valid number");
             }
             scannerInstructor.nextLine(); // handles the next line character
         }
         System.out.println("The total sum is " + sum);
+        System.out.println("The Max is " + max);
+        System.out.println("The Min is " + min);
         scannerInstructor.close();
     }
 }
