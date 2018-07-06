c093735292a24d640329eaef69ff2da9029a9c31
GabrielPora <gabriel.groener@gmail.com>
2018-07-06 13:56:38 +0200

    Abstract Class Challenge Part 2 completed

--- Udemy_java/AbstractClassChallenge/src/com/gabriel/Main.java
+++ Udemy_java/AbstractClassChallenge/src/com/gabriel/Main.java
@@ -3,6 +3,41 @@ package com.gabriel;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        MyLinkedList list = new MyLinkedList(null);
+        list.traverse(list.getRoot());
+
+//        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        String stringData = "0 1 2 3 4 5 6 7 8 9";
+
+        String[] data = stringData.split(" ");
+        for (String s : data) {
+            // create new item with value set to the string s
+            list.addItem(new Node(s));
+        }
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("3"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("5"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("0"));
+        list.removeItem(new Node("4"));
+        list.removeItem(new Node("2"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("9"));
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("8"));
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("6"));
+        list.traverse(list.getRoot());
+        list.removeItem(list.getRoot());
+        list.traverse(list.getRoot());
+        list.removeItem(list.getRoot());
+        list.traverse(list.getRoot());
+
+
     }
 }
