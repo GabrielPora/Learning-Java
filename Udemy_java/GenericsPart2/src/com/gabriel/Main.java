3c30203d6c373f315671633aad4c862153783dc5
GabrielPora <gabriel.groener@gmail.com>
2018-07-09 13:19:42 +0200

    Naming Conventions and Packages. static and final keywords Packages Part 1

--- Udemy_java/GenericsPart2/src/com/gabriel/Main.java
+++ Udemy_java/GenericsPart2/src/com/gabriel/Main.java
@@ -1,9 +1,5 @@
 package com.gabriel;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-
 public class Main {
 
     public static void main(String[] args) {
