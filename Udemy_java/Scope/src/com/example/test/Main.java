0d1f338d1ccb87217bf01a260fb5d9a3f438c908
GabrielPora <gabriel.groener@gmail.com>
2018-07-10 09:34:10 +0200

    Scope Part1 & Part 2 and Visibility completed

---
+++ Udemy_java/Scope/src/com/example/test/Main.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String varFour = "This is private to main";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        scopeInstance.useInner();
+        System.out.println("scopeInstance var1 is "+ scopeInstance.getVarOne());
+        System.out.println("String varFour is "+varFour);
+
+        scopeInstance.timesTwo();
+        System.out.println("========================================");
+        ScopeCheck.InnerClss innerClss = scopeInstance.new InnerClss();
+        innerClss.timesTwo();
+    }
+}
