4b5afe42ad85efa8af7b12103c27aad714ee38ae
GabrielPora <gabriel.groener@gmail.com>
2018-07-11 15:34:44 +0200

    Add a Basket

--- Udemy_java/SortedCollections/src/com/gabriel/StockList.java
+++ Udemy_java/SortedCollections/src/com/gabriel/StockList.java
@@ -1,24 +1,25 @@
 package com.gabriel;
 
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StockList {
     private final Map<String, StockItem> list;
 
     public StockList() {
-        this.list = new HashMap<>();
+        this.list = new LinkedHashMap<>();
     }
 
     public int addStock(StockItem item) {
         if (item != null) {
-            // check if already have quantities of this item,
+            // check if already have quantities of this item
             StockItem inStock = list.getOrDefault(item.getName(), item);
-            // if there are already stock on this item adjust the quantity
+            // If there are already stocks on this item, adjust the quantity
             if (inStock != item) {
                 item.adjustStock(inStock.quantityInStock());
             }
+
             list.put(item.getName(), item);
             return item.quantityInStock();
         }
@@ -27,6 +28,7 @@ public class StockList {
 
     public int sellStock(String item, int quantity) {
         StockItem inStock = list.getOrDefault(item, null);
+
         if ((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0)) {
             inStock.adjustStock(-quantity);
             return quantity;
@@ -48,11 +50,14 @@ public class StockList {
         double totalCost = 0.0;
         for (Map.Entry<String, StockItem> item : list.entrySet()) {
             StockItem stockItem = item.getValue();
+
             double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+
             s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
-            s = s + itemValue + "\n";
+            s = s + String.format("%.2f", itemValue) + "\n";
             totalCost += itemValue;
         }
+
         return s + "Total stock value " + totalCost;
     }
 }
